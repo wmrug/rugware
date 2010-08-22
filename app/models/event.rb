4b1043c2d0bd4ab872eb052f75a8e70982f6028c
Omar Qureshi <omaraliqureshi@googlemail.com>
2010-08-22 01:55:59 +0100

    Fix bug where not checking for start_time > end_time

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,14 @@ class Event < ActiveRecord::Base
   DEFAULT_MEETING_LENGTH = 2.hours
   validates_presence_of :title, :body, :start_time, :end_time
   validates_uniqueness_of :title, :case_sensitive => false
+  
+  validate :end_time_should_be_after_start_time
+  
+  private
+  
+    def end_time_should_be_after_start_time
+      self.attributes.inspect
+      errors.add(:end_time, "Must be after start time") if start_time && end_time && start_time > end_time
+    end
+  
 end
--- test/unit/event_test.rb
+++ test/unit/event_test.rb
@@ -7,4 +7,11 @@ class EventTest < ActiveSupport::TestCase
   should validate_presence_of(:title)
   should validate_presence_of(:start_time)
   should validate_presence_of(:end_time)
+  
+  should "not be able to create an event with a start_time > end_time" do
+    assert_raises ActiveRecord::RecordInvalid do
+      Factory(:event, :start_time => Time.now + 1.hour, :end_time => Time.now)
+    end
+  end
+  
 end
