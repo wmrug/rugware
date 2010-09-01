8002af2012c16a3efb6dfbfb9a7712e9119d7658
Adre Du Toit <adre.dutoit@foxsoft.net>
2010-09-01 23:53:43 +0100

    created application helper methods to abstract js code out of application layout for better clarity

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,22 @@
 module ApplicationHelper
+  def countdown_script_for_next_event
+    @time = next_event_datetime
+    "//<![CDATA[
+        jQuery(document).ready(function() {
+          $('#countdown_dashboard').countDown({
+            targetDate: {
+              'day':#{@time[3]}, 'month':#{@time[4]}, 'year': #{@time[5]}, 'hour': #{@time[2]}, 'min': #{@time[1]}, 'sec': #{@time[0]}
+            }
+          });
+
+          $('#email_field').focus(email_focus).blur(email_blur);
+          $('#subscribe_form').bind('submit', function() { return false; });
+        });
+      //]]>"
+  end
+  
+  private
+  def next_event_datetime
+    Event.upcoming.first.start_time.to_time.to_a
+  end
 end
