673ae93a416fc1ad6de5d1df2f8199da7b34c4f9
Andy Henson <andrew.henson@foxsoft.net>
2010-09-01 21:56:29 +0100

    Order past events in descending order so most recent are at the top of the list

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,7 +5,7 @@ class Event < ActiveRecord::Base
   
   validate :end_time_should_be_after_start_time
 
-  scope :past, where("events.start_time IS NOT NULL AND events.start_time <= now()")
+  scope :past, where("events.start_time IS NOT NULL AND events.start_time <= now()").order("start_time DESC")
   scope :upcoming, where("events.start_time IS NOT NULL AND events.start_time > now()")
   
   private
