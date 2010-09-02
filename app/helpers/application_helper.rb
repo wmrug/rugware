5f6357cc48c7a2afa9ce3a2d9ac2a2a2307307fb
Adre Du Toit <adre.dutoit@foxsoft.net>
2010-09-02 22:42:45 +0100

    moved script tags back into layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,21 +1,19 @@
 module ApplicationHelper
   def countdown_script_for_next_event
     time = next_event_datetime
-    "<script type=\"text/javascript\">
-      //<![CDATA[
-          jQuery(document).ready(function() {
-            $('#countdown_dashboard').countDown({
-              targetDate: {
-                'day':#{time[3]}, 'month':#{time[4]}, 'year': #{time[5]}, 'hour': #{time[2]}, 'min': #{time[1]}, 'sec': #{time[0]}
-              }
-            });
+    "//<![CDATA[
+        jQuery(document).ready(function() {
+          $('#countdown_dashboard').countDown({
+            targetDate: {
+              'day':#{time[3]}, 'month':#{time[4]}, 'year': #{time[5]}, 'hour': #{time[2]}, 'min': #{time[1]}, 'sec': #{time[0]}
+            }
           });
-        //]]>
-      </script>"
+        });
+      //]]>"
   end
   
   private
   def next_event_datetime
-    Event.upcoming.first.start_time.to_time.to_a
+    Event.upcoming.first.start_time.to_a
   end
 end
