f52e3db7bc878310fcf33c887f270ff0fc5aeed7
Adre Du Toit <adre.dutoit@foxsoft.net>
2010-09-03 09:24:53 +0100

    added completion event to show that the event has begun

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,7 +6,13 @@ module ApplicationHelper
           $('#countdown_dashboard').countDown({
             targetDate: {
               'day':#{time[3]}, 'month':#{time[4]}, 'year': #{time[5]}, 'hour': #{time[2]}, 'min': #{time[1]}, 'sec': #{time[0]}
+            },
+            // onComplete function
+            onComplete: function() {
+              $('#complete_info_message').slideDown(),
+              $('.countdown_label').fadeOut()
             }
+            
           });
         });
       //]]>"
