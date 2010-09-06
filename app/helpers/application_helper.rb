e813617b71cb53374f658e88febbdd9fdb511135
Adre Du Toit <adre.dutoit@foxsoft.net>
2010-09-06 23:09:46 +0100

    abstracted script tags for countdown into partial for better readability

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,21 +1,23 @@
 module ApplicationHelper
   def countdown_script_for_next_event
     time = @next_event.start_time.to_a
-    "//<![CDATA[
-        jQuery(document).ready(function() {
-          $('#countdown_dashboard').countDown({
-            targetDate: {
-              'day':#{time[3]}, 'month':#{time[4]}, 'year': #{time[5]}, 'hour': #{time[2]}, 'min': #{time[1]}, 'sec': #{time[0]}
-            },
-            // onComplete function
-            onComplete: function() {
-              $('#complete_info_message').slideDown(),
-              $('.countdown_label').fadeOut()
-            }
+    "<script type=\"text/javascript\">
+      //<![CDATA[
+          jQuery(document).ready(function() {
+            $('#countdown_dashboard').countDown({
+              targetDate: {
+                'day':#{time[3]}, 'month':#{time[4]}, 'year': #{time[5]}, 'hour': #{time[2]}, 'min': #{time[1]}, 'sec': #{time[0]}
+              },
+              // onComplete function
+              onComplete: function() {
+                $('#complete_info_message').slideDown(),
+                $('.countdown_label').fadeOut()
+              }
             
+            });
           });
-        });
-      //]]>"
+        //]]>
+      </script>"
   end
   
 end
